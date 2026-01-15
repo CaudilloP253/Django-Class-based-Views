34826a3b845d5ab7f8d497c09f40e23b4b3e742e
CaudilloP253 <luiscaudillop@gmail.com>
2026-01-15 02:06:16 -0600

    Update models.py
    added "user" key for class product

--- src/products/models.py
+++ src/products/models.py
@@ -1,6 +1,9 @@
 from django.db import models
+from django.conf import settings
 
+User=settings_AUTH_USER_MODEL
 class Product(models.Model):
+    user = models.ForeingKey(User, blank=True, null=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=120)
     slug= models.SlugField(unique=True)
 
